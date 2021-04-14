9614c181ad78123f5e2d9798b30475998b0b80e3
Xuejun Yang <xuejya@microsoft.com>
2021-04-14 19:20:19 +0000

    Test pipeline pre-shell-PR
    Signed-off-by: Xuejun Yang <xuejya@microsoft.com>

--- tests/libc/run.c
+++ tests/libc/run.c
@@ -21,7 +21,7 @@ static void _run_tests(const char* test_file)
         fprintf(stderr, "File %s not found \n", test_file);
     }
     char line[256];
-
+    int i = 1;
     while (fgets(line, sizeof(line), file))
     {
         line[strlen(line) - 1] = '\0';
@@ -29,7 +29,7 @@ static void _run_tests(const char* test_file)
         pid_t pid;
         int wstatus;
 
-        printf("=== start test: %s\n", line);
+        printf("=== start test %d: %s\n", i++, line);
 
         /* skip lines that begin with '#' */
         if (line[0] == '#')
